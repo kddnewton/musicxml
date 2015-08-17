04525f4361b449b6468fc0e88769612dc5f0635a
Kevin Deisz <kevin.deisz@gmail.com>
2015-08-17 19:27:54 -0400

    better tests and console

--- test/musicxml/node_test.rb
+++ test/musicxml/node_test.rb
@@ -12,4 +12,17 @@ class NodeTest < Minitest::Test
     assert MusicXML::Node.const_defined?(:TestNode)
     assert_equal 'test', MusicXML::Node::TestNode.echo('test')
   end
+
+  def test_registration
+    constants = [:Base, :Attributes, :Clef, :Defaults, :Encoding, :Identification, :Measure, :Note, :Notations,
+      :Part, :PartList, :Pitch, :ScoreInstrument, :ScorePart, :Key, :ScorePartwise]
+
+    constants.each do |constant|
+      assert MusicXML::Node.const_defined?(constant)
+
+      klass = MusicXML::Node.const_get(constant)
+      assert klass.respond_to?(:stored_attributes)
+      assert klass.respond_to?(:stored_nodes)
+    end
+  end
 end
