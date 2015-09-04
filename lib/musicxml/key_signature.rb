b68dc6b2d2b3b27d2e837d7818c3e70d5d6d21b0
Evan Callaway <evanmcallaway@gmail.com>
2015-09-03 23:55:55 -0400

    Add support for key signature in Lilypond output

--- lib/musicxml/key_signature.rb
+++ lib/musicxml/key_signature.rb
@@ -12,5 +12,15 @@ module MusicXML
     def humanize
       FIFTHS[number + 6]
     end
+
+    def lilypond_name
+      # WTF lilypond.  W. T. F.
+      self.humanize.gsub('b', 'es').gsub('#', 'is').downcase
+    end
+
+    def to_lilypond
+      #TODO: add support for minor keys
+      "\\key #{self.lilypond_name} \\major" 
+    end
   end
 end
