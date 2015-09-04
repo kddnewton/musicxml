f39da9c34f6ad0ba208b90adf15718d5f075cb33
Kevin Deisz <kevin.deisz@gmail.com>
2015-09-04 17:28:40 -0400

    privatizing lilypond_name and adding tests for to_lilypond on key signature

--- test/musicxml/key_signature_test.rb
+++ test/musicxml/key_signature_test.rb
@@ -15,4 +15,15 @@ class KeySignatureTest < Minitest::Test
     key_signature = MusicXML::KeySignature.new(2)
     assert_equal 'D', key_signature.humanize
   end
+
+  def test_to_lilypond
+    key_signature = MusicXML::KeySignature.new(0)
+    assert_equal "\\key c \\major", key_signature.to_lilypond
+
+    key_signature = MusicXML::KeySignature.new(6)
+    assert_equal "\\key fis \\major", key_signature.to_lilypond
+
+    key_signature = MusicXML::KeySignature.new(-2)
+    assert_equal "\\key bes \\major", key_signature.to_lilypond
+  end
 end
